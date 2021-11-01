f578625172f9a575fea4cdf580f40423c82c1b79
huyvu2001 <41413347+huyvu2001@users.noreply.github.com>
2021-11-01 23:32:36 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const Express = require("express");
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(
+	`postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+	{
+		logging: false,
+	}
+);
+var app = Express();
+
+app.listen(process.env.PORT || 8080, () => {
+	console.log("Server running!");
+});
+
+app.get("/", (req, res) => {
+	res.status(200).send("Eazylearn backend");
+});
