3141be9905e0d86e62ebcaf412f5c2a7bcddeb53
huyvu2001 <41413347+huyvu2001@users.noreply.github.com>
2021-11-26 20:28:54 +0700

    put, assign & remove students & lecturer api added

--- route/course_routes.js
+++ route/course_routes.js
@@ -2,15 +2,69 @@ const router = require("express").Router();
 
 const auth = require("../middleware/auth");
 const CourseService = require("../service/course_service");
-
+const Error = require("../model/error");
 const courseService = new CourseService();
+
+router.post("/", auth, async (req, res) => {
+	try {
+		const type = req.user.type;
+		if (type !== 0) throw new Error("Unauthorized", 401);
+		const result = await courseService.createCourse(req.body);
+		return res.status(200).json({ status: "OK", message: result });
+	} catch (err) {
+		return res.status(err.statusCode).json(err);
+	}
+});
+
+router.post("/assign/student", auth, async (req, res) => {
+	try {
+		const type = req.user.type;
+		if (type !== 0) throw new Error("Unauthorized", 401);
+		const result = await courseService.assignStudentToCourse(req.body);
+		return res.status(200).json({ status: "OK", message: result });
+	} catch (err) {
+		return res.status(err.statusCode).json(err);
+	}
+});
+
+router.post("/assign/lecturer", auth, async (req, res) => {
+	try {
+		const type = req.user.type;
+		if (type !== 0) throw new Error("Unauthorized", 401);
+		const result = await courseService.assignLecturerToCourse(req.body);
+		return res.status(200).json({ status: "OK", message: result });
+	} catch (err) {
+		return res.status(err.statusCode).json(err);
+	}
+});
 
 router.get("/", auth, async (req, res) => {
 	try {
+		const courseID = req.query.id;
+		if (courseID != null) {
+			const result = await courseService.getCourseByID(courseID);
+			return res.status(200).json({ status: "OK", course: result });
+		}
 		const semester = req.query.semester;
-		const courses = await courseService.getCourseBySemseter(semester, req.user);
+		const courses = await courseService.getCourseBySemester(semester, req.user);
 		return res.status(200).json({ status: "OK", courses: courses });
 	} catch (err) {
+		return res.status(err.statusCode).json(err);
+	}
+});
+
+router.put("/", auth, async (req, res) => {
+	try {
+		const type = req.user.type;
+		if (type !== 0) throw new Error("Unauthorized", 401);
+
+		const courseID = req.query.id;
+		if (courseID == null) throw new Error("Bad request", 401);
+		console.log("here");
+		const result = await courseService.updateCourseByID(courseID, req.body);
+		return res.status(200).json({ status: "OK", message: result });
+	} catch (err) {
+		console.log(err.statusCode);
 		return res.status(err.statusCode).json(err);
 	}
 });
@@ -18,12 +72,37 @@ router.get("/", auth, async (req, res) => {
 router.delete("/", auth, async (req, res) => {
 	try {
 		const type = req.user.type;
+		if (type !== 0) throw new Error("Unauthorized", 401);
 		const id = req.query.id;
 		const response = await courseService.deleteCourseByID(id, type);
 		return res.status(200).json({
 			status: "OK",
 			message: response + " course deleted successfully",
 		});
+	} catch (err) {
+		return res.status(err.statusCode).json(err);
+	}
+});
+
+router.delete("/remove/student", auth, async (req, res) => {
+	try {
+		const type = req.user.type;
+		if (type !== 0) throw new Error("Unauthorized", 401);
+		const result = await courseService.removeStudentFromCourse(req.body);
+		if (result == 0) throw new Error("Not found", 404);
+		return res.status(200).json({ status: "OK", message: result });
+	} catch (err) {
+		return res.status(err.statusCode).json(err);
+	}
+});
+
+router.delete("/remove/lecturer", auth, async (req, res) => {
+	try {
+		const type = req.user.type;
+		if (type !== 0) throw new Error("Unauthorized", 401);
+		const result = await courseService.removeLecturerFromCourse(req.body);
+		if (result == 0) throw new Error("Not found", 404);
+		return res.status(200).json({ status: "OK", message: result });
 	} catch (err) {
 		return res.status(err.statusCode).json(err);
 	}
--- service/course_service.js
+++ service/course_service.js
@@ -4,7 +4,76 @@ const Error = require("../model/error");
 const courseRepository = new CourseRepository();
 
 class CourseService {
-	async getCourseBySemseter(sem, user) {
+	async createCourse(course) {
+		try {
+			const { id, name, academicYear, semester } = course;
+			if (
+				id == null ||
+				name == null ||
+				academicYear == null ||
+				semester == null ||
+				semester < 0 ||
+				semester > 3
+			)
+				throw new Error("Bad request", 400);
+			const result = await courseRepository.createCourse(
+				id,
+				name,
+				academicYear,
+				semester
+			);
+			return result;
+		} catch (err) {
+			if (err.statusCode == null) throw new Error(err, 500);
+			throw new Error(err.message, err.statusCode);
+		}
+	}
+
+	async assignStudentToCourse(body) {
+		const { studentID, courseID } = body;
+		if (studentID == null || courseID == null)
+			throw new Error("Bad request", 401);
+		try {
+			const result = await courseRepository.assignStudentToCourse(
+				studentID,
+				courseID
+			);
+			return result;
+		} catch (err) {
+			console.log(err);
+			if (err.statusCode == null) throw new Error(err, 500);
+			throw new Error(err.message, err.statusCode);
+		}
+	}
+
+	async assignLecturerToCourse(body) {
+		const { lecturerID, courseID } = body;
+		if (lecturerID == null || courseID == null)
+			throw new Error("Bad request", 401);
+		try {
+			const result = await courseRepository.assignLecturerToCourse(
+				lecturerID,
+				courseID
+			);
+			return result;
+		} catch (err) {
+			if (err.statusCode == null) throw new Error(err, 500);
+			throw new Error(err.message, err.statusCode);
+		}
+	}
+
+	async getCourseByID(id) {
+		try {
+			const result = await courseRepository.getCourseByID(id);
+			if (result == null) throw new Error("Not found", 404);
+			return result;
+		} catch (err) {
+			if (err.statusCode == null) throw new Error(err, 500);
+			throw new Error(err.message, err.statusCode);
+		}
+	}
+
+	async getCourseBySemester(sem, user) {
 		try {
 			var result;
 			if (user.type === 2)
@@ -14,23 +83,80 @@ class CourseService {
 				);
 			else if (user.type === 1)
 				result = await courseRepository.getCourseByLecturerID(user.account_id);
+
 			return result;
 		} catch (err) {
-			throw Error(err[0].message, 500);
+			if (err.statusCode == null) throw new Error(err, 500);
+			throw new Error(err.message, err.statusCode);
 		}
 	}
 
-	async deleteCourseByID(id, type) {
-		if (type !== 0) throw new Error("Unauthorized", 401);
+	async updateCourseByID(qid, course) {
+		const { id, name, academicYear, semester } = course;
+		if (
+			qid == null ||
+			id == null ||
+			name == null ||
+			academicYear == null ||
+			semester == null ||
+			semester < 0 ||
+			semester > 3
+		)
+			throw new Error("Bad request", 401);
 		try {
-			const result = await Course.destroy({
-				where: {
-					course_id: id,
-				},
-			});
+			const result = await courseRepository.updateCourseByID(
+				qid,
+				id,
+				name,
+				academicYear,
+				semester
+			);
 			return result;
 		} catch (err) {
-			throw Error(err[0].message, 500);
+			if (err.statusCode == null) throw new Error(err, 500);
+			throw new Error(err.message, err.statusCode);
+		}
+	}
+
+	async deleteCourseByID(id) {
+		try {
+			const result = await courseRepository.deleteCourseByID(id);
+			return result;
+		} catch (err) {
+			if (err.statusCode == null) throw new Error(err, 500);
+			throw new Error(err.message, err.statusCode);
+		}
+	}
+
+	async removeStudentFromCourse(body) {
+		const { studentID, courseID } = body;
+		if (studentID == null || courseID == null)
+			throw new Error("Bad request", 401);
+		try {
+			const result = await courseRepository.removeStudentFromCourse(
+				studentID,
+				courseID
+			);
+			return result;
+		} catch (err) {
+			if (err.statusCode == null) throw new Error(err, 500);
+			throw new Error(err.message, err.statusCode);
+		}
+	}
+
+	async removeLecturerFromCourse(body) {
+		const { lecturerID, courseID } = body;
+		if (lecturerID == null || courseID == null)
+			throw new Error("Bad request", 401);
+		try {
+			const result = await courseRepository.removeLecturerFromCourse(
+				lecturerID,
+				courseID
+			);
+			return result;
+		} catch (err) {
+			if (err.statusCode == null) throw new Error(err, 500);
+			throw new Error(err.message, err.statusCode);
 		}
 	}
 }
