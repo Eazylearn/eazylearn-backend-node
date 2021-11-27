17367d81500004c2e1e59ec9503e160b1b7ddf47
vt132 <54886268+vt132@users.noreply.github.com>
2021-11-28 03:31:04 +0700

    create, get/update/delete by id question

--- route/index.js
+++ route/index.js
@@ -3,9 +3,11 @@ const router = Express.Router();
 
 const accountRoutes = require("./account_routes");
 const courseRoutes = require("./course_routes");
+const questionRoutes = require("./question_routes")
 
 router.use("/account", accountRoutes);
 router.use("/course", courseRoutes);
+router.use("/question", questionRoutes);
 // more routes here
 
 module.exports = router;
