f3c0e909b31201c403157e7fa91fb208fa8fb081
vt132 <54886268+vt132@users.noreply.github.com>
2021-11-29 22:41:13 +0700

    quiz basic CRUD

---
+++ repository/quiz_repository.js
@@ -0,0 +1,88 @@
+const Quiz = require("../model/quiz");
+const QuizQuestion = require("../model/quiz_question");
+
+class QuizRepository {
+	async getQuizIDByCourseID(courseID) {
+		try {
+			const quizID = await Quiz.findAll({
+				where: {
+					course_id:courseID,
+				},
+				
+			});
+
+			const result = [];
+			quizID.forEach((c) => result.push(c.quizID));
+			return result;
+		} catch (err) {
+			console.log(err);
+			throw err;
+		}
+	}
+    async getQuizByID(ID) {
+		try {
+			const quiz = await Quiz.findAll({
+				where: {
+					quiz_id: getQuizIDByID(ID),
+				},
+				
+			});
+
+			const result = [];
+			quiz.forEach((c) => result.push(c.quiz));
+			return result;
+		} catch (err) {
+			console.log(err);
+			throw err;
+		}
+	}
+    async deleteQuizByID(id) {
+		try {
+			const result = await Quiz.destroy({
+				where: {
+					quiz_id: id,
+				},
+			});
+			return result;
+		}  catch (err) {
+			console.log(err);
+			throw err;
+		}
+	}
+	
+	async createQuizByID(id, name,timeLỉmit,courseID) {
+		try {
+			const result = await Quiz.create({
+				quiz_id: id,
+			    quiz_name:name,
+                time_lỉmit:timeLỉmit,
+                course_id:courseID,
+			});
+			return result;
+		} catch (err) {
+			throw err;
+		}
+	}
+	async updateQuizByID(id, name, timeLimit, courseID){
+		try {
+			const result = await Course.update(
+				{
+					quiz_id: id,
+                    quiz_name:name,
+                    time_lỉmit:timeLimit,
+                    course_id:courseID,
+				},
+				{
+					where: {
+						quiz_id: id,
+					},
+				}
+			);
+			return result;
+		} catch (err) {
+			throw err;
+		}
+	}	
+}
+
+module.exports = QuizRepository;
