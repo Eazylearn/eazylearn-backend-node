40d8dc31f0972e67d99fd0465965582c3e65073c
vt132 <54886268+vt132@users.noreply.github.com>
2022-01-07 21:28:04 +0700

    Revert "Update student_service.js"
    This reverts commit 07a3043ecc179c8597d2c3e5bbd1f9a38c83c944.

--- service/student_service.js
+++ service/student_service.js
@@ -1,3 +1,4 @@
+const QuestionRepository = require("../repository/student_repository");
 const Error = require("../model/error");
 const studentRepository = require("../repository/student_repository");
 
